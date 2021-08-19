167e3348f8a98c48c90d083f78b4d07402ff3a18
Luis Felipe Klaus <duckbox@hotmail.com>
2021-08-19 17:59:44 -0300

    back to original

--- shopping/shopping.py
+++ shopping/shopping.py
@@ -59,7 +59,6 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    print("TODO HERE")
     raise NotImplementedError
 
 
