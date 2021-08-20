9f6778b81273122797804cb5d7554de08bb7a103
Luis Felipe Klaus <duckbox@hotmail.com>
2021-08-20 18:24:44 -0300

    load_data implemented

--- shopping/shopping.py
+++ shopping/shopping.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
@@ -9,6 +10,7 @@ TEST_SIZE = 0.4
 
 def main():
 
+    
     # Check command-line arguments
     if len(sys.argv) != 2:
         sys.exit("Usage: python shopping.py data")
@@ -59,7 +61,47 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+    # Use pandas to read data from csv file and load to a dataframe
+    df = pd.read_csv(filename, header=0)
+   
+    # Create a month chart and assign the corresponding value to each month entry
+    month_chart = {
+        'Jan': 0, 'Feb': 1, 'Mar': 2, 'Apr': 3,
+        'May': 4, 'June': 5, 'Jul': 6, 'Aug': 7,
+        'Sep': 8, 'Oct': 9, 'Nov': 10, 'Dec': 11  
+    }
+    
+    for month, value in month_chart.items():
+        df.Month.replace(month, value, inplace=True)
+    
+    # Assign 0 for each 'not returning' and 1 for each 'returning' visitor
+    df.VisitorType.replace('Returning_Visitor', 1, inplace=True)
+    df.VisitorType.replace('New_Visitor', 0, inplace=True)
+    # NOTE: As directed in project specifications VisitorType 
+    # should be 1 for returning visitors and 0 for non-returning 
+    # visitors. Therefore, the 85 entries labeled 'Other' in the
+    # dataset were considered non-returning for that matter (0).
+    df.VisitorType.replace('Other', 0, inplace=True)
+
+    # Convert updated columns to int (bool type columns 0 or 1 assignment is automatic)
+    df = df.astype({'Month': int, 'VisitorType': int, 'Weekend': int, 'Revenue': int})
+    #df.info()
+    
+    # Populate evidence and labels lists 
+    evidence = list() 
+    labels = list() 
+    columns = df.columns
+    
+    for i in range(len(df)):
+        row_data = list()        
+        
+        for column in columns:
+            row_data.append(df[column][i])
+
+        evidence.append(row_data[0:-1])
+        labels.append(row_data[-1])
+   
+    return (evidence, labels)
 
 
 def train_model(evidence, labels):
